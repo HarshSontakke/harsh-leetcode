84adbc066334addfa5210c665839483d0dce7f63
harsh <155796102+HarshSontakke@users.noreply.github.com>
2026-08-26 12:53:55 +0530

    Time: 17 ms (78.15%), Space: 45.7 MB (31.35%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -5,7 +5,7 @@ class Solution {
         int closestSum = nums[0] + nums[1] + nums[2]; // initialize
 
         for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+          
 
 
             int left = i + 1, right = n - 1;
