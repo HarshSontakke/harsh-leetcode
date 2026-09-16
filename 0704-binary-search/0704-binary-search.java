3ebe1e539be05818495daaa9e2fa0a17836c2735
harszy <155796102+HarshSontakke@users.noreply.github.com>
2026-09-16 18:13:34 +0530

    Time: 0 ms (100%), Space: 48.5 MB (13.94%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -6,8 +6,8 @@ class Solution {
         while(low <= high){
             int mid = (low + high)/2;
             if(nums[mid]==target) return mid;
-            else if(target > nums[mid]) low = mid+1;
-            else high = mid - 1;
+            else if (target > nums[mid]) low = mid +1 ;
+            else high = mid -1 ;
         }
         return -1;
     }
